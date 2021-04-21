a88aa0e3f14da0a85ed6d41f40f276716d489826
“[Nils <[gaul@fzi.de]>
2021-04-21 20:09:28 +0200

    Started to implement an approach to solve task 5 by accessing folder which contains all orders

--- src/more_interfaces/src/amr_node.cpp
+++ src/more_interfaces/src/amr_node.cpp
@@ -6,33 +6,62 @@
 #include "more_interfaces/msg/order.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 
+#include <iostream>
+#include <vector>
+#include <dirent.h>
+#include <fstream>
+
+#include <yaml-cpp/yaml.h>
+
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+std::vector<char *> extract_files(const char * path){
+  DIR *dir; struct dirent *diread;
+  std::vector<char *> files;
+
+  if ((dir = opendir(path)) != nullptr) {
+      while ((diread = readdir(dir)) != nullptr) {
+          files.push_back(diread->d_name);
+      }
+      closedir (dir);
+  } else {
+      perror ("opendir");
+  }
+  return files;
+}
+
+
+//Task 1
 class RobotNode : public rclcpp::Node
 {
 public:
   RobotNode()
   : Node("OrderOptimizer")
   {
+    /*
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
-    500ms, std::bind(&RobotNode::timer_callback, this));
+    1000ms, std::bind(&RobotNode::timer_callback, this));
+    */
+
 
     subscription_ = this->create_subscription<std_msgs::msg::String>(
       "topic", 10, std::bind(&RobotNode::topic_callback, this, _1));
+    
 
     //Task 2
     subscription_current_position = this->create_subscription<geometry_msgs::msg::PoseStamped>(
       "currentPosition", 10, std::bind(&RobotNode::currentPosition_callback, this, _1));
 
+    //Using custom message type "more_interfaces::msg::Order"
     subscription_next_order = this->create_subscription<more_interfaces::msg::Order>(
       "nextOrder", 10, std::bind(&RobotNode::nextOrder_callback, this, _1));
 
     //Task 3
-    this->declare_parameter<std::string>("path_parameter", "/home/nils/knapp_ws/src/files");
-      timer_path_param = this->create_wall_timer(
-      1000ms, std::bind(&RobotNode::respond, this));
+    this->declare_parameter<std::string>("path_parameter", "/home/nils/knapp_ws/src/files/orders");
+    timer_path_param = this->create_wall_timer(
+      10000ms, std::bind(&RobotNode::respond, this));
 
   }
 
@@ -71,7 +100,33 @@ private:
 
   void nextOrder_callback(const more_interfaces::msg::Order::SharedPtr msg) const
   {
-    RCLCPP_INFO(this->get_logger(), "I heard at nextOrder_callback: '%s' !", msg->description);
+    RCLCPP_INFO(this->get_logger(), "nextOrder_callback: id: '%i', description: '%s' !", msg->order_id, msg->description.c_str());
+
+    //Task 5
+    //Iterate through all files in 'orders' folder
+    std::vector<char *> files = extract_files(path_param.c_str());
+
+    for (auto file : files){
+      std::cout << file << "| ";
+      std::cout << std::endl;
+
+      //Check for information related to given order
+
+      //auto doc = YAML::LoadFile(std::string(file));
+      /*
+      std::string text_line;
+      std::ifstream current_file(file);
+      
+      
+      while (getline (current_file, text_line)) {
+      // Output the text from the file
+      std::cout << text_line;
+      }
+      
+      current_file.close();
+      */
+    } 
+  
   }
 
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
